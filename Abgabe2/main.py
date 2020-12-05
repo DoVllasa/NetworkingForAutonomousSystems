e692020d63e340f67d0ba6f74bc2d905d6ceb411
Niklas Polster <niklas.polster@pedepe.de>
2020-12-05 18:10:12 +0100

    Abgabe 2 Flooding finished

--- Abgabe2/main.py
+++ Abgabe2/main.py
@@ -1,27 +1,38 @@
 from xbee import transmit, receive, atcmd
 import json
+import utime
 
+save_identifier = {}
 
-# class Packet:
-#
-#     def __init__(self, objectType, source, destination, identifier):
-#         self.objectType = objectType
-#         self.source = source
-#         self.destination = destination
-#         self.identifier = identifier
+millis = utime.ticks_ms()
 
+while True:
+    current_millis = utime.ticks_ms()
 
-# while True:
-#    msg = receive()
-#    if msg:
-#        print(json.loads(msg['payload']))
-#        break
+    if current_millis - millis >= 30000:
+        print('Reset save identifier')
+        millis = current_millis
+        save_identifier = {}
 
-packet = {
-    'objectType': 1,
-    'source': 2,
-    'destination': 3,
-    'identifier': 4,
-}
-
-transmit(0xFFFF, json.dumps(packet))
+    msg = receive()
+    if msg:
+        packet = json.loads(msg['payload'])
+        objectType = packet['objectType']
+        destination = packet['destination']
+        source = packet['source']
+        identifier = packet['identifier']
+        source_address_self = ''.join('{:02x}'.format(x).upper() for x in atcmd("BL"))
+        if destination in source_address_self:
+            if identifier not in save_identifier:
+                ack_packet = {
+                    'objectType': 1,
+                    'source': source_address_self,
+                    'destination': source,
+                    'identifier': identifier,
+                }
+                transmit(0xFFFF, json.dumps(ack_packet))
+                save_identifier[identifier] = ack_packet['objectType']
+        else:
+            if packet['identifier'] not in save_identifier or save_identifier[identifier] != objectType:
+                save_identifier[identifier] = objectType
+                transmit(0xFFFF, json.dumps(packet))
